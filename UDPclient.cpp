fbc0495d1fb957526d06bd86e0b257fa371b7670
SPlkv <73382208+SPlkv@users.noreply.github.com>
2021-04-25 10:18:12 +0500

    Add files via upload

---
+++ UDPclient.cpp
@@ -0,0 +1,78 @@
+﻿
+#include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+void main()
+{
+	string ipAddress = "127.0.0.1";			// IP
+	int port = 1234;						// порт
+
+
+	// WinSock
+	WSAData data;
+	WORD ver = MAKEWORD(2, 2);
+	int wsResult = WSAStartup(ver, &data);
+	if (wsResult != 0)
+	{
+		cerr << "Can't start Winsock, Err #" << wsResult << endl;
+		return;
+	}
+
+	SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		cerr << "Can't create socket, Err #" << WSAGetLastError() << endl;
+		WSACleanup();
+		return;
+	}
+
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(port);
+	inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+	int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));			// Подключение к серверу
+	if (connResult == SOCKET_ERROR)
+	{
+		cerr << "Can't connect to server, Err #" << WSAGetLastError() << endl;
+		closesocket(sock);
+		WSACleanup();
+		return;
+	}
+	// Отправка и получение данных
+	char buf[4096];
+	string userInput;
+
+	do
+	{
+		cout << "> ";
+		getline(cin, userInput);
+
+		if (userInput.size() > 0)
+		{
+			// Отправка текста
+			int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+			if (sendResult != SOCKET_ERROR)
+			{
+
+				// Ожидание ответа
+				ZeroMemory(buf, 4096);
+				int bytesReceived = recv(sock, buf, 4096, 0);
+				if (bytesReceived > 0)
+				{
+
+
+					cout << "SERVER> " << string(buf, 0, bytesReceived) << endl;
+				}
+			}
+		}
+
+	} while (userInput.size() > 0);
+
+	closesocket(sock);			// Закрытие сокета
+	WSACleanup();
+}
